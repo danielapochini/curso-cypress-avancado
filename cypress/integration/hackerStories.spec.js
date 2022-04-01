9df863bd750ae13c05a3474be950990601cf2985
danielapochini <daniela.pochini@bs2tecnologia.com.br>
2022-04-01 17:42:45 -0300

    Exercício Extra Credit 2.2

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -26,7 +26,7 @@ describe('Hacker Stories', () => {
     // TODO: Find a way to test it out.
     it.skip('shows the right data for all rendered stories', () => {})
 
-    it.only('shows 20 stories, then the next 20 after clicking "More"', () => {
+    it('shows 20 stories, then the next 20 after clicking "More"', () => {
       cy.intercept({
         method: 'GET',
         pathname: '**/search',
@@ -81,6 +81,11 @@ describe('Hacker Stories', () => {
     const newTerm = 'Cypress'
 
     beforeEach(() => {
+      cy.intercept( 
+        'GET', 
+        `**/search?query=${newTerm}&page=0`
+      ).as('getNewTermStories')
+
       cy.get('#search')
         .clear()
     })
@@ -89,7 +94,7 @@ describe('Hacker Stories', () => {
       cy.get('#search')
         .type(`${newTerm}{enter}`)
 
-      cy.assertLoadingIsShownAndHidden()
+      cy.wait('@getNewTermStories')
 
       cy.get('.item').should('have.length', 20)
       cy.get('.item')
